24816d82861f37e2e6001508b080e468183ba80c
digitalflowerstudio <floriantappeser@gmail.com>
2024-01-18 16:45:34 +0100

    updated functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,96 @@
-import { useState } from 'react';
-import './App.css'
-import MovieContainer from './assets/components/movieContainer/MovieContainer'
-import movies from './assets/data/data'
-import Button from './assets/components/button/Button';
+import { useState } from "react";
+import "./App.css";
+import MovieContainer from "./assets/components/movieContainer/MovieContainer";
+import data from "./assets/data/data";
 
 function App() {
-  // const [dateAscending, setDateAscending] = useState(false)
-  // console.log(movies);
+  const [movie, setMovies] = useState(data);
+  console.log(movie);
+  const [search, setSearch] = useState("");
 
+  const sortAscending = () => {
+    let newMovieArray = [...movie];
+    let sortedMovieArray = newMovieArray.sort((a, b) => {
+      return a.year - b.year;
+    });
+    setMovies(sortedMovieArray);
+  };
 
-  // const sortDateAsc = (data) => {
-  //   console.log("Data: ", data);
-  //   data.sort((a,b) => a.year - b.year)
-  //   console.log("Sortiertes Array: ", data);
+  const sortDescending = () => {
+    let newMovieArray = [...movie];
+    let sortedMovieArray = newMovieArray.sort((a, b) => {
+      return b.year - a.year;
+    });
+    setMovies(sortedMovieArray);
+  };
 
-  }
+  const sortRateAscending = () => {
+    let newMovieArray = [...movie];
+    let sortedMovieArray = newMovieArray.sort((a, b) => {
+      return a.rate - b.rate;
+    });
+    setMovies(sortedMovieArray);
+  };
+
+  const sortRateDescending = () => {
+    let newMovieArray = [...movie];
+    let sortedMovieArray = newMovieArray.sort((a, b) => {
+      return b.rate - a.rate;
+    });
+    setMovies(sortedMovieArray);
+  };
+
+  const searchFunc = () => {
+    let newMovieArray = [...movie];
+    let result = newMovieArray.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setMovies(result);
+  };
+
+  const reset = () => {
+    setMovies(data);
+  };
   return (
     <>
       <header>
         <nav>
-          {/* <button onClick={""}>
-            Sort by Date Ascending
-          </button> */}
-          {/* <Button 
-            name={"Sort by Date Ascending"}
-            func={function() { 
-              sortDateAsc(movies)
-            }}
-          />
-          <Button 
-            name={"Sort by Date Descending"}
-          />
-          <Button 
-            name={"Best Rate"}
-          />
-          <Button 
-            name={"A-Z"}
-          />
-          <Button 
-            name={"Z-A"}
-          /> */}
-        <Button />
-
+          <button onClick={sortAscending}>Sort Year Up</button>
+          <button onClick={sortDescending}>Sort Year Down</button>
+          <button onClick={sortRateAscending}>Sort Rating Up</button>
+          <button onClick={sortRateDescending}>Sort Rating Down</button>
+          <button onClick={reset}>Reset</button>
         </nav>
-        <form action="#">
-          <input 
-          type="text" 
-          name="" 
-          id="" 
-          placeholder='Filmtitle'
-          onClick={''}
-          />
-        </form>
+
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Filmtitle"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button onClick={searchFunc}>Search</button>
       </header>
       <main>
-        {
-          movies.map((movie, index) => {
-            return (
-              <div key={index} className='movieContainer'>
-                <MovieContainer
-                  title={movie.title}
-                  director={movie.director}
-                  year={movie.year}
-                  duration={movie.duration}
-                  genre={movie.genre}
-                  rate={movie.rate}
-                  />
-              </div>
-            )
-          })
-        }
+        {movie.map((movie, index) => {
+          return (
+            <div key={index} className="movieContainer">
+              <MovieContainer
+                title={movie.title}
+                director={movie.director}
+                year={movie.year}
+                duration={movie.duration}
+                genre={movie.genre}
+                rate={movie.rate}
+              />
+            </div>
+          );
+        })}
         {/* {sortDateAsc(movies)} */}
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
